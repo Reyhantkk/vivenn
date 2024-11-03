06e4abc1cee41c4642777bb02ba7fa8e0e17f1de
Reyhan tok <120533118+Reyhantkk@users.noreply.github.com>
2024-11-03 16:15:18 +0300

    .

--- mesaj_gnder.php
+++ mesaj_gnder.php
@@ -1,16 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Form verilerini alıyoruz
-    $name = $_POST['fullname'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
+    $name = htmlspecialchars($_POST['fullname']);  // Güvenlik için girişleri temizliyoruz
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);
 
-    // E-posta bilgileri
-    $to = "seninmailadresi@ornek.com";  // Kendi e-posta adresini buraya yaz
+    // Hedef e-posta adresi
+    $to = "davut@vovienn.com, info@vovienn.com";
+     // Buraya kendi e-posta adresinizi yazın
     $subject = "İletişim Formu Mesajı";
     $body = "Ad Soyad: $name\nEmail: $email\nMesaj:\n$message";
     
-    // Gönderenin e-posta adresini başlık kısmına ekleyelim
+    // E-posta başlıklarını ayarlıyoruz
     $headers = "From: $email";
 
     // E-postayı gönderiyoruz
